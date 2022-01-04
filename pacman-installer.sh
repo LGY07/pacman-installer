dd18bbf816fd304ae16ee8a00facaf17441a034a
Tienyu Yang <ellistwooten8269@gmail.com>
2022-01-05 00:24:44 +0800

    Values should be named with upper-case alphabets

--- pacman-installer.sh
+++ pacman-installer.sh
@@ -1,35 +1,33 @@
 #!/usr/bin/env bash
-title="Pacman inatall script"
-version=2022.01.01
-dir=/.pacman
-filename="archlinux-bootstrap-"$version"-x86_64.tar.gz"
-download="http://mirror.rackspace.com/archlinux/iso/"$version"/"$filename
-echo -e '\033]2;'$title'\007'
+RELEASE_DATE=2022.01.01
+INSTALL_DIRECTORY=/.pacman
+DOWNLOAD_FILENAME="archlinux-bootstrap-"$RELEASE_DATE"-x86_64.tar.gz"
+DOWNLOAD_LINK="http://mirror.rackspace.com/archlinux/iso/"$RELEASE_DATE"/"$DOWNLOAD_FILENAME
 printf "\033c"
 if [ $(whoami) == "root" ]
 then echo "Pacman inatall script v2"
 else echo -e "\033[31mERROR:\033[0m Please run this script as root，are you root?" & exit
 fi
 
-echo "rootfs :"$filename
-echo "download form :"$download
+echo "rootfs :"$DOWNLOAD_FILENAME
+echo "download form :"$DOWNLOAD_LINK
 echo "Please wait"
-wget $download || curl -O $download || echo -e "\033[31mERROR:\033[0m Please make sure wget is installed!"
+wget $DOWNLOAD_LINK || curl -O $DOWNLOAD_LINK || echo -e "\033[31mERROR:\033[0m Please make sure wget is installed!"
 rm -f ./log.txt 2>/dev/null
 touch ./log.txt
-tar -xvf "./"$filename 2>./log.txt
+tar -xvf "./"$DOWNLOAD_FILENAME 2>./log.txt
 cp -r ./root.x86_64/etc/pacman.d/ /etc/pacman.d/ 2>>./log.txt
 echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch">./pacman/root.x86_64/etc/pacman.d/archlinuxcn-mirrorlist
 wget https://huashijituan.coding.net/p/liuzhierzhong/d/liuzhierzhong/git/raw/master/pacman.conf || curl -O https://huashijituan.coding.net/p/liuzhierzhong/d/liuzhierzhong/git/raw/master/pacman.conf 2>>./log.txt
 mv ./pacman.conf /etc/pacman.conf 2>>./log.txt
-mv ./root.x86_64/ $dir 2>>./log.txt
-echo "[archlinuxcn]">>$dir"/etc/pacman.conf"
-echo "SigLevel = Optional TrustAll">>$dir"/etc/pacman.conf"
-echo "Include = /etc/pacman.d/archlinuxcn-mirrorlist">>$dir"/etc/pacman.conf"
-ln -sf $dir"/usr/bin/pacman" /usr/bin/t-pacman 2>>./log.txt
+mv ./root.x86_64/ $INSTALL_DIRECTORY 2>>./log.txt
+echo "[archlinuxcn]">>$INSTALL_DIRECTORY"/etc/pacman.conf"
+echo "SigLevel = Optional TrustAll">>$INSTALL_DIRECTORY"/etc/pacman.conf"
+echo "Include = /etc/pacman.d/archlinuxcn-mirrorlist">>$INSTALL_DIRECTORY"/etc/pacman.conf"
+ln -sf $INSTALL_DIRECTORY"/usr/bin/pacman" /usr/bin/t-pacman 2>>./log.txt
 
 echo -e "\033[32mFinish\033[0m ,the number of lines in the log file:"
 wc -l ./log.txt
-$dir"/usr/bin/pacman-key" --init
-pacman -Syyu && echo -e "\033[32mSuccess\033[0m \033[33m use \033[0m "$dir"\033[33m/usr/bin[Software Name] to run the Software was installed by Pacman.\033[0m"
+$INSTALL_DIRECTORY"/usr/bin/pacman-key" --init
+pacman -Syyu && echo -e "\033[32mSuccess\033[0m \033[33m use \033[0m "$INSTALL_DIRECTORY"\033[33m/usr/bin[Software Name] to run the Software was installed by Pacman.\033[0m"
 
